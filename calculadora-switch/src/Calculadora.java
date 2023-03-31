d7d52947896ecce3612d5071705855aea462021e
emanuelbispoc <emanuelbispoc@gmail.com>
2023-03-31 14:27:44 -0300

    revisao

--- calculadora-switch/src/Calculadora.java
+++ calculadora-switch/src/Calculadora.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -10,24 +11,24 @@ public class Calculadora {
         System.out.println("Escolha a operação:\n+\n-\n/\n*\n^");
         String operacao = sc.nextLine();
 
-        Integer numero1 = 0;
-        Integer numero2 = 0;
-        Integer base = 0;
-        Integer expoente = 0;
+        BigDecimal numero1 = BigDecimal.ZERO;
+        BigDecimal numero2 = BigDecimal.ZERO;
+        BigDecimal base = BigDecimal.ZERO;
+        BigDecimal expoente = BigDecimal.ZERO;
 
         if(operacao.equals("^")){
             System.out.println("Digite a base: \n");
-            base = sc.nextInt();
+            base = sc.nextBigDecimal();
 
             System.out.println("Digite o expoente: \n");
-            expoente = sc.nextInt();
+            expoente = sc.nextBigDecimal();
         }else {
 
             System.out.println("Digite o primerio numero: ");
-            numero1 = sc.nextInt();
+            numero1 = sc.nextBigDecimal();
 
             System.out.println("Digite o segundo numero: \n");
-            numero2 = sc.nextInt();
+            numero2 = sc.nextBigDecimal();
 
         }
 
@@ -55,25 +56,25 @@ public class Calculadora {
 
     }
 
-    public static int somar(int numero1, int numero2) {
-        return numero1 + numero2;
+    public static BigDecimal somar(BigDecimal numero1, BigDecimal numero2) {
+        return numero1.add(numero2);
     }
 
-    public static int subtrair(int numero1, int numero2) {
-        return numero1 - numero2;
+    public static BigDecimal subtrair(BigDecimal numero1, BigDecimal numero2) {
+        return numero1.subtract(numero2);
     }
 
-    public static int dividir(int numero1, int numero2) {
-        return numero1 / numero2;
+    public static BigDecimal dividir(BigDecimal numero1, BigDecimal numero2) {
+        return numero1.divide(numero2);
     }
 
-    public static int multiplicar(int numero1, int numero2) {
-        return numero1 * numero2;
+    public static BigDecimal multiplicar(BigDecimal numero1, BigDecimal numero2) {
+        return numero1.multiply(numero2);
     }
 
-    public static int potencia(int base, int expoente) {
-        if(expoente == 0) return 1;
-        return base * potencia(base, expoente - 1);
+    public static BigDecimal potencia(BigDecimal base, BigDecimal expoente) {
+        if(expoente.equals(BigDecimal.valueOf(0))) return BigDecimal.ONE;
+        return base.multiply(potencia(base, expoente.subtract(BigDecimal.valueOf(1))));
     }
 
 }
--- calculadora-switch/test/CalculadoraTest.java
+++ calculadora-switch/test/CalculadoraTest.java
@@ -1,32 +1,45 @@
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculadoraTest {
     @Test
     void deveRetornarASomaDeDoisComTres() {
-        assertEquals(5, Calculadora.somar(2, 3));
+        assertEquals(BigDecimal.valueOf(5),
+                Calculadora.somar(BigDecimal.valueOf(2), BigDecimal.valueOf(3))
+        );
     }
 
     @Test
     void deveRetornarASubtracaoDeCincoComTres() {
-        assertEquals(2, Calculadora.subtrair(5, 3));
+        assertEquals(BigDecimal.valueOf(2),
+                Calculadora.subtrair(BigDecimal.valueOf(5), BigDecimal.valueOf(3))
+        );
     }
 
     @Test
     void deveRetornarADivisaoDeOitoComDois() {
-        assertEquals(4, Calculadora.dividir(8, 2));
+        assertEquals(BigDecimal.valueOf(4),
+                Calculadora.dividir(BigDecimal.valueOf(8), BigDecimal.valueOf(2))
+        );
     }
 
     @Test
     void deveRetornarAMultiplicacaoDeQuatroComCinco() {
-        assertEquals(20, Calculadora.multiplicar(4, 5));
+        assertEquals(BigDecimal.valueOf(20),
+                Calculadora.multiplicar(BigDecimal.valueOf(4), BigDecimal.valueOf(5)));
     }
 
     @Test
     void deveRetornarAPotenciacao() {
-        assertEquals(16, Calculadora.potencia(4, 2));
-        assertEquals(125, Calculadora.potencia(5, 3));
+        assertEquals(BigDecimal.valueOf(16),
+                Calculadora.potencia(BigDecimal.valueOf(4), BigDecimal.valueOf(2))
+        );
+        assertEquals(BigDecimal.valueOf(125),
+                Calculadora.potencia(BigDecimal.valueOf(5), BigDecimal.valueOf(3))
+        );
     }
 
 }
